44015cf9d2f85f98a5586e1289f3860b30756269
ryanspandu <1910036@gmail.com>
2021-03-21 16:45:12 +0700

    meta viewport

--- head.php
+++ head.php
@@ -13,6 +13,9 @@
 <script type="text/javascript" src="node_modules/jquery/dist/jquery.min.js"></script>
 <script type="text/javascript" src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 
-
+<!-- META -->
 <meta name="robots" content="noindex, nofollow">
 <meta name="googlebot" content="noindex">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
