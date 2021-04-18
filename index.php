3b2ab73f2b65e51c2dcbbff2278a3e90169df739
ryanspandu <1910036@gmail.com>
2021-04-18 18:23:49 +0700

    head update

--- index.php
+++ index.php
@@ -3,6 +3,29 @@
 <head>
     <title>Melzha & Rizki - Wedding Invitation</title>
     <?php include 'head.php'; ?>
+
+    <!-- CSS -->
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com"/>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css2?family=Yellowtail&display=swap" rel="stylesheet"/>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+
+    <link rel="icon" href="assets/img/favicon2.svg"/>
+
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css"/>
+    <link href="assets/css/jquery.countdown.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/font/style.css"/>
+    <link rel="stylesheet" href="assets/css/style.css"/>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+
+    <!-- JS -->
+    <script type="text/javascript" src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script type="text/javascript" src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.countdown.min.js"></script>
 </head>
 <body>
     <?php include 'modal/_welcome.php'; ?>
