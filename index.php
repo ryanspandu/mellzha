3069fa5504a1d9fdaebf7b999f9a1aa0219b02d7
ryanspandu <1910036@gmail.com>
2021-04-18 14:32:30 +0700

    section 1 update

--- index.php
+++ index.php
@@ -1,12 +1,29 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Wedding Invitation - Rizky & Melzha</title>
+    <title>Melzha & Rizki - Wedding Invitation</title>
     <?php include 'head.php'; ?>
 </head>
 <body>
     <?php include 'modal/_welcome.php'; ?>
-    
+    <div class="container-fluid px-0">
+
+        <!-- Section 1 -->
+        <div class="bg-main-container" style="background-image: url('assets/img/bg-main-container.jpg');">
+            <div class="bg-main-hide-container w-100 h-100 d-flex flex-column align-items-center justify-content-end text-white pb-5">
+                <p class="">WE ARE GETTING MARRIED</p>
+                <h1 class="signature3">
+                    Melzha & Rizki
+                </h1>
+                <p class="">06 JUNE 2021</p>
+            </div>
+        </div>
+
+        <!-- Section 2 -->
+        <div class="bg-blue-main py-5">
+            asds
+        </div>
+    </div>
 </body>
 
 <!-- Script JS -->
