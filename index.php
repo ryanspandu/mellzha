5f036e5e5755e72ebb7e2a89ebaaa8a51f122825
ryanspandu <1910036@gmail.com>
2021-04-18 18:56:34 +0700

    update

--- index.php
+++ index.php
@@ -6,69 +6,7 @@
 </head>
 <body>
     <?php include 'modal/welcome.php'; ?>
-    <div class="container-fluid px-0 d-none">
-
-        <!-- Section 1 -->
-        <div class="bg-main-container" style="background-image: url('assets/img/bg-main-container.jpg');">
-            <div class="bg-main-hide-container w-100 h-100 d-flex flex-column align-items-center justify-content-end text-white pb-5">
-                <p class="">WE ARE GETTING MARRIED</p>
-                <h1 class="signature3">
-                    Melzha & Rizki
-                </h1>
-                <p class="">06 JUNE 2021</p>
-            </div>
-        </div>
-
-        <!-- Section 2 -->
-        <div class="bg-blue-main pb-4 section2">
-            <div class="container-lg d-flex flex-column align-items-center text-white">
-                <img src="assets/img/002-bouquet.svg" width="90px" class="mb-4"/>
-                <p class="text-center mt-3 text-light" style="max-width: 550px;">
-                    “And of His signs is that He created for you from yourselves mates that you may find tranquillity in them; and He placed between you affection and mercy. Indeed in that are signs for a people who give thought.”
-                </p>
-                <p class="text-center mt-2 text-light">
-                    ( QS. Ar- Rum 21 )
-                </p>
-            </div>
-        </div>
-
-        <!-- Section 3 -->
-        <div class="section3 py-5">
-            <div class="container-lg py-4">
-                <div class="row text-blue-main">
-                    <div class="col-12 col-md-4 text-center">
-                        <h4 class="">Demelzha</h4>
-                        <p class="mb-0">Putri dari</p>
-                        <p class="mb-0">Bapak</p>
-                        <p class="mb-0">& Ibu</p>
-                    </div>
-                    <div class="col-12 col-md-4 text-center">
-                        <p class="font-weight-bold signature3 text-center" style="font-size: 100px;">&</p>
-                    </div>
-                    <div class="col-12 col-md-4 text-center">
-                        <h4 class="">Rizki</h4>
-                        <p class="mb-0">Putra dari</p>
-                        <p class="mb-0">Bapak</p>
-                        <p class="mb-0">& Ibu</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Section 4 -->
-        <div class="section4 bg-blue-second py-5">
-            <div class="container-lg py-4 text-white">
-                <div class="row">
-                    <div class="col-12 text-center">
-                        <h1 class="signature3">Save The Date</h1>
-                    </div>
-                    <div class="col-12 text-center">
-                        <div id="countdown"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php include 'content.php'; ?>
 </body>
 
 <!-- Script JS -->
