f6263367b21ef87ea9ef543ca024a4752827398e
ryanspandu <1910036@gmail.com>
2021-04-18 11:57:05 +0700

    favicon & modal update

--- modal/_welcome.php
+++ modal/_welcome.php
@@ -1,18 +1,20 @@
-<div class="modal-welcome position-fix fixed-top w-100 h-100 opening-bg-img d-flex flex-column justify-content-center align-items-center" style="background-image: url('assets/img/bg.jpg'); ">
-    <div class="position-absolute w-100 h-100 modal-bg"> 
-    </div>
-    <div class="w-100 d-flex flex-column justify-content-center align-items-center" style="z-index:2;">
-        <div class="text-white text-center mb-5">
-            <h2 class="font-weight-bold">
-                You're invited!
-            </h2>
-            <h4 class="font-weight-bold mb-4">
-                The Wedding Celebration of 
-            </h4>
-            <h1 class="signature3">
-                Melzha & Rizki
-            </h1>
+<div class="container">
+    <div class="modal-welcome position-fix fixed-top w-100 h-100 opening-bg-img d-flex flex-column justify-content-center align-items-center" style="background-image: url('assets/img/bg.jpg'); ">
+        <div class="position-absolute w-100 h-100 modal-bg"> 
         </div>
-        <button class="btn font-weight-bold open-invitation"><i class="fas fa-heart mr-3"></i>Open Invitation</button>
+        <div class="w-100 d-flex flex-column justify-content-center align-items-center px-3 px-lg-0" style="z-index:2;">
+            <div class="text-white text-center mb-5">
+                <h2 class="font-weight-bold">
+                    You're invited!
+                </h2>
+                <h4 class="font-weight-bold mb-4">
+                    The Wedding Celebration of 
+                </h4>
+                <h1 class="signature3">
+                    Melzha & Rizki
+                </h1>
+            </div>
+            <button class="btn font-weight-bold open-invitation shadow"><i class="fas fa-heart mr-3"></i>Open Invitation</button>
+        </div>
     </div>
 </div>
