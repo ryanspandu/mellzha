c2b2551707f9cdb64a31dbfc1d6cca7ba55a3057
ryanspandu <1910036@gmail.com>
2021-04-18 19:35:25 +0700

    update

--- head.php
+++ head.php
@@ -5,20 +5,20 @@
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
 
-<link rel="icon" href="../assets/img/favicon2.svg"/>
+<link rel="icon" href="/assets/img/favicon2.svg"/>
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-<link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css"/>
-<link rel="stylesheet" href="../assets/font/style.css"/>
-<link rel="stylesheet" href="../assets/css/style.css"/>
+<link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css"/>
+<link rel="stylesheet" href="/assets/font/style.css"/>
+<link rel="stylesheet" href="/assets/css/style.css"/>
 
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
 
 <!-- JS -->
-<script type="text/javascript" src="../node_modules/jquery/dist/jquery.min.js"></script>
-<script type="text/javascript" src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="/node_modules/jquery/dist/jquery.min.js"></script>
+<script type="text/javascript" src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 
 <!-- META -->
 <meta name="robots" content="noindex, nofollow">
