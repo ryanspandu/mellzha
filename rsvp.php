a0be320d7c610cb7ba249c7e226d73f20ec36bc5
ryanspandu <1910036@gmail.com>
2021-05-26 05:55:30 +0000

    rsvp

--- rsvp.php
+++ rsvp.php
@@ -2,18 +2,31 @@
 // koneksi database
 include 'conn.php';
 ini_set('date.timezone', 'Asia/Jakarta');
+
+function randomStr($length = 5) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return strtoupper($randomString);
+}
  
-// menangkap data yang di kirim dari form
+$id= date('Ymdhis').randomStr();
 $email = $_POST['email'];
 $name = $_POST['name'];
 $msg = $_POST['msg'];
 $created_at = date('Y-m-d H:i:s');
 $updated_at = date('Y-m-d H:i:s');
- 
-// menginput data ke database
-mysqli_query($conn,"insert into rsvp values('','$email','$name','$msg','$created_at','$updated_at')");
- 
-// mengalihkan halaman kembali ke index.php
-header("location:index.php");
+
+
+$query = mysqli_query($conn, "INSERT INTO rsvp VALUES ('$id','$email','$name','$msg','$created_at','$updated_at')");
+if ($query) {
+    header("location:index.php");
+} else{ 
+    echo mysqli_error($conn);
+
+}
  
 ?>
